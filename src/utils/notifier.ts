2d809b803108ced0882c417ca760125c8b85cd02
Wen Zhu <hitzhuwen@gmail.com>
2023-04-09 10:49:51 -0500

    trying to solve the greeting not received issue

--- src/utils/notifier.ts
+++ src/utils/notifier.ts
@@ -6,8 +6,8 @@ export default class Notifier {
   constructor() {
     this.transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,  // true for 465, false for other ports
+      port: 465,
+      secure: true,  // true for 465, false for other ports
       auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_PASS,
