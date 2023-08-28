33db989a706f94b09d37722fc829cc8a0843f81e
Wen Zhu <hitzhuwen@gmail.com>
2023-08-27 19:59:32 -0500

    support on3 2025 predictions

--- src/sites/on3.ts
+++ src/sites/on3.ts
@@ -3,7 +3,10 @@ import {Detector} from '@/utils/detector';
 import * as cheerio from 'cheerio';
 
 
-const FETCH_URL = 'https://www.on3.com/db/expert-predictions/football/2024/';
+const FETCH_URLS = [
+  'https://www.on3.com/db/expert-predictions/football/2024/',
+  'https://www.on3.com/db/expert-predictions/football/2025/',
+];
 
 export default class On3Detector {
   private readonly detector: Detector;
@@ -15,49 +18,54 @@ export default class On3Detector {
   }
 
   async detect() {
-    const data = await this.detector.load(FETCH_URL);
+    const allPredictions = await Promise.all(FETCH_URLS.map(async url => {
+      const data = await this.detector.load(url);
 
-    const $ = cheerio.load(data);
+      const $ = cheerio.load(data);
 
-    const predictions =
-        $('ul[class^="PredictionCenterList_predictionListWrapper__"] li:has([class^="PredictionCenterItem_predictorWrapper"])')
-            .map((_, item) => {
-              const item$ = cheerio.load(item);
-              const playerName =
-                  item$('[class^="PredictionCenterItem_playerName"] span')
-                      .text();
-              const expertName =
-                  item$(
-                      '[class^="PredictionCenterItem_predictorInfoWrapper"] a')
-                      .text();
-              const predictionTime =
-                  item$(
-                      '[class^="PredictionCenterItem_dateTime"],[class*=" PredictionCenterItem_dateTime"]')
-                      .text();
-              const isFlipping =
-                  item$(
-                      '[class^="PredictionCenterItem_predictionSwappedIcon__"]')
-                      .length > 0;
-              const predictions =
-                  item$('img[class^="PredictionCenterItem_teamLogo"]')
-                      .map((_, teamImg) => $(teamImg).attr('title') ?? '')
-                      .map(
-                          (_, teamName) =>
-                              this.capitalizeFirstLetters(teamName));
+      return $('ul[class^="PredictionCenterList_predictionListWrapper__"] li:has([class^="PredictionCenterItem_predictorWrapper"])')
+          .map((_, item) => {
+            const item$ = cheerio.load(item);
+            const playerName =
+                item$('[class^="PredictionCenterItem_playerName"] span').text();
+            const expertName =
+                item$('[class^="PredictionCenterItem_predictorInfoWrapper"] a')
+                    .text();
+            const predictionTime =
+                item$(
+                    '[class^="PredictionCenterItem_confidenceDateContainer__"] [class*=" PredictionCenterItem_dateTime"]')
+                    .last()
+                    .text();
+            const isFlipping =
+                item$('[class^="PredictionCenterItem_predictionSwappedIcon__"]')
+                    .length > 0;
+            const predictions =
+                item$('img[class^="PredictionCenterItem_teamLogo"]')
+                    .map((_, teamImg) => $(teamImg).attr('title') ?? '')
+                    .map(
+                        (_, teamName) => this.capitalizeFirstLetters(teamName));
 
-              return {
-                playerKey: playerName,
-                playerName,
-                expertKey: expertName,
-                expertName,
-                predictionDate: predictionTime,
-                prediction: isFlipping ? predictions[1] : predictions[0],
-              };
-            })
-            .get()
-            .filter(isPrediction);
+            return {
+              playerKey: playerName,
+              playerName,
+              expertKey: expertName,
+              expertName,
+              predictionDate: new Date(predictionTime).toISOString(),
+              prediction: isFlipping ? predictions[1] : predictions[0],
+            };
+          })
+          .get()
+          .filter(isPrediction);
+    }));
 
-    await this.detector.compareAndNotify(predictions);
+    const sortedPredictions =
+        allPredictions.flat()
+            .sort(
+                (p1, p2) => new Date(p2.predictionDate).getTime() -
+                    new Date(p1.predictionDate).getTime())
+            .slice(0, 20);
+
+    await this.detector.compareAndNotify(sortedPredictions);
   }
 
   private capitalizeFirstLetters(str: string) {
