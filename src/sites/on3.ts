70ad0e3ac85362d6ad02c68182fce9bd3b45b3c2
Wen Zhu <hitzhuwen@gmail.com>
2023-12-24 13:41:03 -0600

    chore: use 2025 on3 prediction url as the default one

--- src/sites/on3.ts
+++ src/sites/on3.ts
@@ -14,7 +14,7 @@ export default class On3Detector {
   constructor() {
     this.detector = new Detector(
         'On3 RPM', process.env.MONGODB_DB_ON3_COLLECTION!,
-        'https://www.on3.com/db/expert-predictions/football/2024/');
+        'https://www.on3.com/db/expert-predictions/football/2025/');
   }
 
   async detect() {
